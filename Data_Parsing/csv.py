e9f008c95ffb8c0d7f78daec58d85920e57ae267
Mike Clarke <mikedclarke109@gmail.com>
2014-09-15 19:34:58 +0100

    Skeleton for csv script

---
+++ Data_Parsing/csv.py
@@ -0,0 +1,15 @@
+"""
+Author: Mike Clarke
+
+Python script for parsing csv files into Json and database.
+"""
+from sys import argv
+import csv
+
+ORIG_CSV_LOC = argv[1]
+
+def parse(csv_file, delimiter):
+	"""Parses CSV file into Json object"""
+	
+
+	return parsed_data
