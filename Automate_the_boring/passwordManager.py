b017b4b472bf6aba842a408b0cb1745cd7d31e92
mikeyy109 <mikedclarke109@gmail.com>
2016-09-18 22:22:21 +0100

    finished pw script

--- Automate_the_boring/passwordManager.py
+++ Automate_the_boring/passwordManager.py
@@ -3,3 +3,16 @@
 
 PASSWORDS = {'email': 'F8dskk73h3Bsj8S', 'blog': 'nsSj7629Sab7G3l', 'other': 12345}
 
+import sys, pyperclip
+
+if len(sys.argv) < 2:
+	print('Usage: python3 passwordManager.py [account]')
+	sys.exit()
+
+account = sys.argv[1]
+
+if account in PASSWORDS:
+	pyperclip.copy(PASSWORDS[account])
+	print('Password for ' + account + ' copied to clipboard..')
+else:
+	print('There is no account named ' + account)
