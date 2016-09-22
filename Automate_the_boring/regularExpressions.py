4f718c8621d51252d9fe31ec55212289802d87f6
mikeyy109 <mikedclarke109@gmail.com>
2016-09-22 21:36:59 +0100

    basic regex

--- Automate_the_boring/regularExpressions.py
+++ Automate_the_boring/regularExpressions.py
@@ -1,3 +1,5 @@
+import re
+
 # Finding UK/US numbers in any text input
 
 def isUKNum(text):
@@ -28,9 +30,10 @@ def isUSNum(text):
 			return False
 	return True
 
-message = "This is a text message, first number 07837492044," \
+message = "-- This is a text message, first number 07837492044," \
 		" 0172319382, 283881, 07281, 07372849533, 555-222-5555," \
-		" 22-333-4444-222, 000-222-1111 test case over."
+		" 22-333-4444-222, 000-222-1111. +447291039488 224.567.3233 " \
+		"922-203-4444 ex 32 07283940459 ---- END ----"
 
 for i in range(len(message)):
 	chunkUK = message[i:i+11]
@@ -41,5 +44,13 @@ for i in range(len(message)):
 		print("US Number found: " + chunkUS)
 print("Done")
 
+# Regular Expressions
 
+rxUS = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+dataUS = rxUS.search(message)
+print(dataUS.group(0))
+
+rxUK = re.compile(r'07(\d){9}')
+dataUK = rxUK.search(message)
+print(dataUK.group(0))
 
